7cbe417ca2af6a92eb168b373b3da7737b661774
Valdemar <valdemar.kobelis@gmail.com>
2023-11-06 14:55:06 +0200

    Add method for computer to place ships on the board

--- src/computer_class.js
+++ src/computer_class.js
@@ -4,7 +4,43 @@ export default class Computer {
   }
 
   getCoordinates() {
-    return this.possibleMoves.splice((Math.random() * 99), 1)
+    return this.possibleMoves.splice((Math.floor(Math.random() * 100)), 1)
+  }
+
+  placeShips(board) {
+    [5, 4, 3, 3, 2].forEach((length) => {
+      vertical = Math.random() > 0.5 ? true : false
+      const coordinates = this.#getPlacementCoordinates(board, vertical, length)
+      board.placeShip(coordinates[0], coordinates[1], length, vertical)
+    })
+  }
+
+  #getPlacementCoordinates(board, vertical, length) {
+    let x = null
+    let y = null
+    let valid = true
+    do {
+      if (vertical) {
+        x = Math.floor(Math.random()*10)
+        y = Math.floor(Math.random()*10) - length + 1
+        for (let i = 0; i < length; i++) {
+          if (board.notEmpty(x, y + i)) {
+            valid = false
+          }
+        }
+      } else {
+        y = Math.floor(Math.random()*10)
+        x = Math.floor(Math.random()*10) - length + 1
+        for (let i = 0; i < length; i++) {
+          if (board.notEmpty(x + i, y)) {
+            valid = false
+          }
+        }
+      }
+      if (valid) {
+        return [x, y]
+      }
+    } while(true)
   }
 
   #resetMoves() {
