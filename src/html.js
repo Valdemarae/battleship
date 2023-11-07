3efd0b828b89ed8bd01495fd070fc32dd384fc10
Valdemar <valdemar.kobelis@gmail.com>
2023-11-07 17:01:43 +0200

    Create method that lets user place a ship on the board

---
+++ src/html.js
@@ -0,0 +1,180 @@
+import Ship from './ship_class'
+import GameBoard from './game_board_class'
+import Player from './player_class'
+import Computer from './computer_class'
+import Game from './game_class'
+
+const EMPTY = ''
+const MISS = 'o'
+const HIT = 'x'
+const SHIP = 's'
+
+const playerBoard = document.querySelector('.player_board')
+const computerBoard = document.querySelector('.computer_board')
+const container = document.querySelector('.container')
+const ships = document.querySelector('.ships')
+
+export default class Html {
+  static populateBoards() {
+    for (let i = 9; i >= 0; i--) {
+      const line = document.createElement('div')
+      const line2 = document.createElement('div')
+      line.classList.add('line')
+      line2.classList.add('line')
+      for (let j = 0; j < 10; j++) {
+        const square = document.createElement('div')
+        const square2 = document.createElement('div')
+        square.classList.add('square', i + '' + j)
+        square2.classList.add('square', i + '' + j)
+        line.appendChild(square)
+        line2.appendChild(square2)
+      }
+      playerBoard.appendChild(line)
+      computerBoard.appendChild(line2)
+    }
+  }
+
+  static async dragShip(length) {
+    const ship = document.createElement('div')
+    ship.classList.add('ship')
+    ship.classList.add('vertical')
+    ship.setAttribute('draggable', true)
+    let width = 4
+    let height = 6.7 * length
+    ship.setAttribute('style', 'width: '+width+'vw; height: '+height+'vh;')
+    document.querySelector('.ships').appendChild(ship)
+    const playerBoard = document.querySelector(".player_board");
+    
+    ship.ondragstart = function(e){
+      e.dataTransfer.clearData();
+      e.dataTransfer.setData("text", e.target.classList[0]);
+    }
+    
+    playerBoard.ondragover = function(e){
+      e.preventDefault();
+      if (e.target.classList[1]) {
+        let y = e.target.classList[1][0]
+        let x = e.target.classList[1][1]
+        if (!ship.classList.contains('vertical')) {
+          for (let i = 0; i < length; i++) {
+            if ((document.getElementsByClassName(y+x)[0])) {
+              (document.getElementsByClassName(y+x)[0]).classList.add('dragover');
+            }
+            x = (Number(x) + 1).toString()
+          }
+        } else {
+          for (let i = 0; i < length; i++) {
+            if (document.getElementsByClassName(y+x)[0]) {
+              (document.getElementsByClassName(y+x)[0]).classList.add('dragover');
+            }
+            y = (Number(y) + 1).toString()
+          }
+        }
+      }
+    }
+    
+    playerBoard.ondragleave = function(e){
+      e.preventDefault();
+      if (e.target.classList[1]) {
+        let y = e.target.classList[1][0]
+        let x = e.target.classList[1][1]
+        if (!ship.classList.contains('vertical')) {
+          for (let i = 0; i < length; i++) {
+            if (document.getElementsByClassName(y+x)[0]) {
+              (document.getElementsByClassName(y+x)[0]).classList.remove('dragover');
+            }
+            x = (Number(x) + 1).toString()
+          }
+        } else {
+          for (let i = 0; i < length; i++) {
+            if (document.getElementsByClassName(y+x)[0]) {
+              document.getElementsByClassName(y+x)[0].classList.remove('dragover');
+            }
+            y = (Number(y) + 1).toString()
+          }
+        }
+      }
+    }
+    
+    document.addEventListener('keydown', (event) => {
+      if (event.key == 'r') {
+        const ship1 = document.querySelector('.ship')
+        if (ship1) {
+          if (ship1.classList.contains('vertical')) {
+            width = 4 * length
+            height = 6.7
+            ship1.classList.remove('vertical')
+          } else {
+            width = 4
+            height = 6.7 * length
+            ship1.classList.add('vertical')
+          }
+          ship1.setAttribute('style', 'width: '+width+'vw; height: '+height+'vh;');
+        }
+      }
+    }, false);
+    
+    playerBoard.ondrop = function(e){
+      e.preventDefault();
+      const data = e.dataTransfer.getData("text");
+      const element = document.querySelector('.'+data)
+      const ships = document.querySelector('.ships')
+      if (validDrop(element, e.target, length)) {
+        let y = e.target.classList[1][0]
+        let x = e.target.classList[1][1]
+        ships.removeChild(element);
+        if (!ship.classList.contains('vertical')) {
+          for (let i = 0; i < length; i++) {
+            (document.getElementsByClassName(y+x)[0]).classList.remove('dragover');
+            (document.getElementsByClassName(y+x)[0]).classList.add('placed');
+            x = (Number(x) + 1).toString()
+          }
+        } else {
+          for (let i = 0; i < length; i++) {
+            (document.getElementsByClassName(y+x)[0]).classList.remove('dragover');
+            (document.getElementsByClassName(y+x)[0]).classList.add('placed');
+            y = (Number(y) + 1).toString()
+          }
+        }
+      } else {
+        let y = e.target.classList[1][0]
+        let x = e.target.classList[1][1]
+        ships.appendChild(element)
+        if (!ship.classList.contains('vertical')) {
+          for (let i = 0; i < length; i++) {
+            if (document.getElementsByClassName(y+x)[0]) {
+              (document.getElementsByClassName(y+x)[0]).classList.remove('dragover');
+            }
+            x = (Number(x) + 1).toString()
+          }
+        } else {
+          for (let i = 0; i < length; i++) {
+            if (document.getElementsByClassName(y+x)[0]) {
+              (document.getElementsByClassName(y+x)[0]).classList.remove('dragover');
+            }
+            y = (Number(y) + 1).toString()
+          }
+        }
+      }
+    }
+    
+    function validDrop(element, pelement, length) {
+      let x = Number(pelement.classList[1][1])
+      let y = Number(pelement.classList[1][0])
+      for (let i = 0; i < length; i++) {
+        if (element.classList.contains('vertical')) {
+          if (y > 9 || document.getElementsByClassName('' + y + x)[0].classList.contains('placed')) {
+            return false
+          }
+          y += 1
+        } else {
+          if (x > 9 || document.getElementsByClassName('' + y + x)[0].classList.contains('placed')) {
+            return false
+          }
+          x += 1
+        }
+      }
+      return true
+    }
+  }
+}
